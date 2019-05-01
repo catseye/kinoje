f046197bc342a0824a01003213ece5602cea0d82
Chris Pressey <cpressey@catseye.tc>
2019-05-01 12:11:18 +0100

    Docker build now uses /mnt/host and PATH for nicer usage.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,13 +2,13 @@
 
 # After building the image, you can run kinoje from within it like:
 #
-# docker run --user $(id -u):$(id -g) -i -t -v "${PWD}:/usr/src/app/host" kinoje \
-#        bin/kinoje host/eg/moebius.yaml -o host/moebius.mp4
+# docker run --user $(id -u):$(id -g) -i -t -v "${PWD}:/mnt/host" kinoje \
+#        kinoje eg/moebius.yaml -o moebius.mp4
 #
 
 rm -rf src/kinoje/*.pyc src/kinoje/__pycache__
 docker container prune
-docker rmi catseye/kinoje:0.5
+docker rmi catseye/kinoje:0.6
 docker rmi kinoje
 docker build -t kinoje .
-docker tag kinoje catseye/kinoje:0.5
+docker tag kinoje catseye/kinoje:0.6
